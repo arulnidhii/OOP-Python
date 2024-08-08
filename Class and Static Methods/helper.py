c14c04e8282ad6b56270732afec8910aaf1e4e8c
Arulnidhi Karunanidhi <81353171+arulnidhii@users.noreply.github.com>
2024-08-08 14:31:18 +0100

    Update helper.py

--- Class and Static Methods/helper.py
+++ Class and Static Methods/helper.py
@@ -1,5 +1,3 @@
-# When to use class methods and when to use static methods ?
-
 class Item:
     @staticmethod
     def is_integer():
@@ -17,11 +15,6 @@ class Item:
         objects, like we have done with CSV.
         '''
 
-# THE ONLY DIFFERENCE BETWEEN THOSE:
-# Static methods are not passing the object reference as the first argument in the background!
-
-
-# NOTE: However, those could be also called from instances.
 
 item1 = Item()
 item1.is_integer()
