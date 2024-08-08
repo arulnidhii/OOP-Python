29b610055de1dccc847733d1e6543704fd2a1a32
Arulnidhi Karunanidhi <81353171+arulnidhii@users.noreply.github.com>
2024-08-08 14:30:27 +0100

    Update main.py

--- Class Inheritance/main.py
+++ Class Inheritance/main.py
@@ -38,8 +38,7 @@ class Item:
 
     @staticmethod
     def is_integer(num):
-        # We will count out the floats that are point zero
-        # For i.e: 5.0, 10.0
+        
         if isinstance(num, float):
             # Count out the floats that are point zero
             return num.is_integer()
